8bcbaa35cb7c75ce914fd2fe2f92e72f54004d3c
prijais1102 <priyajaiswal.mzp1@gmail.com>
2024-01-09 13:26:20 +0530

    Add project files.

---
+++ Day2CSharp/Product.cs
@@ -0,0 +1,53 @@
+﻿namespace Day2CSharp
+{
+    internal class Product
+    {
+        readonly int pCode;
+        string pName;
+        int qtyInStock;
+        float discountAllowed;
+        static string brand;
+
+        public Product()
+        {
+            pCode = 1015;
+        }
+        public Product(int pCode, string pName, int qtyInStock, float discountAllowed)
+        {
+            this.pCode = pCode;
+            this.pName = pName;
+            this.qtyInStock = qtyInStock;
+            this.discountAllowed = discountAllowed;
+        }
+
+        static Product ()
+        {
+            brand = "Lewis";
+        }
+
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Enter Product Name");
+            pName = Console.ReadLine();
+            Console.WriteLine("Enter Quantity in Stock");
+            qtyInStock=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Discount Allowed");
+            discountAllowed = float.Parse(Console.ReadLine());
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine(pCode);
+            Console.WriteLine(pName); 
+            Console.WriteLine(qtyInStock);
+            Console.WriteLine(discountAllowed);
+            Console.WriteLine(brand);
+        }
+        //static void Main(string[] args)
+        //{
+            
+
+        //    Console.WriteLine("Hello, World!");
+        //}
+    }
+}
---
+++ Day2CSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2CSharp
+{
+    enum UserType { Admin=1,Customer=2};
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            Console.WriteLine("1.Admin\n2.Customer");
+            Console.WriteLine("Enter User Type");
+
+            int userType=Byte.Parse(Console.ReadLine());
+            switch(userType) { 
+                case (byte)UserType.Admin: product.GetProductDetails();
+                    break;
+
+                    case (byte)UserType.Customer: product.DisplayDetails();
+                    break;
+            }
+        }
+    }
+}
