3b0f809759c37f8a94e89580a7afa5f465c2ff97
prijais1102 <priyajaiswal.mzp1@gmail.com>
2024-01-09 18:36:18 +0530

    Updates

--- Day2CSharp/Product.cs
+++ Day2CSharp/Product.cs
@@ -1,23 +1,28 @@
-﻿namespace Day2CSharp
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Day2CSharp
 {
     internal class Product
     {
-        readonly int pCode;
+        int pCode;
         string pName;
         int qtyInStock;
         float discountAllowed;
         static string brand;
-
+        double amount;
+        List<Product> products =new List<Product>();
         public Product()
         {
-            pCode = 10105;
+            pCode = 1;
         }
-        public Product(int pCode, string pName, int qtyInStock, float discountAllowed)
+        public Product(int pCode, string pName, int qtyInStock, float discountAllowed , double amount)
         {
             this.pCode = pCode;
             this.pName = pName;
             this.qtyInStock = qtyInStock;
             this.discountAllowed = discountAllowed;
+            this.amount = amount;
         }
 
         static Product ()
@@ -27,27 +32,89 @@
 
         public void GetProductDetails()
         {
-            Console.WriteLine("Enter Product Name");
-            pName = Console.ReadLine();
-            Console.WriteLine("Enter Quantity in Stock");
-            qtyInStock=int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Discount Allowed");
-            discountAllowed = float.Parse(Console.ReadLine());
-        }
-
-        public void DisplayDetails()
-        {
-            Console.WriteLine(pCode);
-            Console.WriteLine(pName); 
-            Console.WriteLine(qtyInStock);
-            Console.WriteLine(discountAllowed);
-            Console.WriteLine(brand);
-        }
-        //static void Main(string[] args)
-        //{
+            //int temp = 0;
+            Console.WriteLine("Enter Product Code");
+            int check = int.Parse(Console.ReadLine());
+            var dup=products.Where(x=>x.pCode == check).ToList();
+            if(dup.Count == 0)
+            {
+                pCode = check;
+                Console.WriteLine("Enter Product Name");
+                pName = Console.ReadLine();
+                Console.WriteLine("Enter Quantity in Stock");
+                qtyInStock = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Discount Allowed");
+                discountAllowed = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter price of product");
+                amount = double.Parse(Console.ReadLine());
+                AddProduct(pCode, pName, qtyInStock, discountAllowed, amount);
+                
+            }
+            else
+            {
+                Console.WriteLine("Duplicate Product Codes not allowed.");
+            }
+           
+        }           
             
+               public void DisplayDetails()
+        {
+                for(var i = 0;i<products.Count;i++)
+                {
+                    Console.WriteLine(products[i].pCode);
+                    Console.WriteLine(products[i].pName);
+                    Console.WriteLine(products[i].qtyInStock);
+                    Console.WriteLine(products[i].discountAllowed);
+                    Console.WriteLine(brand);
+                    Console.WriteLine(amount);
+                }
+        }
 
-        //    Console.WriteLine("Hello, World!");
-        //}
-    }
-}
+        public void AddProduct(int pCode, string pName, int qtyInStock , float discountAllowed, double amount)
+        {
+            products.Add(new Product
+            {
+                pCode = pCode,
+                pName = pName,
+                qtyInStock = qtyInStock,
+                discountAllowed = discountAllowed,
+                amount=amount
+            });
+
+        }
+        public void Purchase(string purchase) { 
+            var list = products.Where(x => x.pName == purchase).ToList();
+            if (list.Count > 0)
+            {
+                foreach (var product in list)
+                {
+                    Console.WriteLine("Product details-->");
+                    Console.WriteLine(pCode);
+                    Console.WriteLine(qtyInStock);
+                    Console.WriteLine(discountAllowed);
+                    Console.WriteLine(brand);
+                    Console.WriteLine(amount);
+                }//foreach
+            }//if
+            else
+            {
+                Console.WriteLine("Product does not exist.");
+            }//else
+            int month= DateTime.Now.Month;
+            int day= DateTime.Now.Day;
+            if (month == 01 && day == 26)
+            {
+                double totalAmount = amount - ((amount * 50) / 100);
+                Console.WriteLine("As today is 26th Jan, Company wants to give a discount of 50% on all the products");
+                Console.WriteLine("The bill is "+totalAmount);
+            }//if
+            else
+            {
+                double totalAmount = amount-((amount * discountAllowed) / 100);
+                Console.WriteLine("The bill is " + totalAmount);
+            }//else
+        }//purchase
+
+            
+        }//class
+    }//namespace
--- Day2CSharp/Program.cs
+++ Day2CSharp/Program.cs
@@ -6,23 +6,55 @@ using System.Threading.Tasks;
 
 namespace Day2CSharp
 {
-    enum UserType { Admin=1,Customer=2};
+    enum UserType { Admin = 1, Customer = 2, End = 3 };
     internal class Program
     {
         static void Main(string[] args)
         {
+            bool condition = true;
+            bool condition1;
+
+
             Product product = new Product();
-            Console.WriteLine("1.Admin\n2.Customer");
+            while (condition)
+            {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1.Admin\n2.Customer\n3.End");
             Console.WriteLine("Enter User Type");
 
-            int userType=Byte.Parse(Console.ReadLine());
-            switch(userType) { 
-                case (byte)UserType.Admin: product.GetProductDetails();
-                    break;
+            int userType = Byte.Parse(Console.ReadLine());
 
-                    case (byte)UserType.Customer: product.DisplayDetails();
-                    break;
-            }
-        }
-    }
-}
+                switch (userType)
+                {
+                    case (byte)UserType.Admin:
+                        product.GetProductDetails();
+                        condition1 = true;
+                        while (condition1)
+                        {
+                            Console.WriteLine("Do you want to add more items(y/n) ?");
+                            char ch = char.Parse(Console.ReadLine());
+                            switch (ch)
+                            {
+                                case 'y':
+                                    product.GetProductDetails();
+                                    break;
+                                case 'n':
+                                    condition1 = false;
+                                    break;
+                            }
+                        }
+                        product.DisplayDetails();
+                            break;
+                        
+                    case (byte)UserType.Customer:
+                        Console.WriteLine("Enter Product Name to Purchase");
+                        string purchase=Console.ReadLine();
+                        product.Purchase(purchase);
+                        break;
+                    case (byte)UserType.End:condition=false;
+                        break;
+                }//switch
+            }//while
+        }//main
+    }//class
+}//namespace
